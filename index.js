b55ddbcb69a3a58e1f13149bb49094521c435545
priyanshk20 <khandelwalpriyansh85@gmail.com>
2021-08-18 12:41:12 +0530

    fixed bugs

--- index.js
+++ index.js
@@ -5,52 +5,37 @@ const errorDiv = document.querySelector(".error");
 const noOfNotes= document.querySelectorAll(".noOfNotes");
 const arrayNoteAmt = [2000, 500, 100, 20, 10, 5, 1];
 
-checkBtn.addEventListener('click', ()=>{
-    clearNoOfNotes();
-    let billAmtValue= Number(bill.value);
-    let cashGivenValue= Number(cash.value);
+checkBtn.addEventListener('click', () => {
+   hideMessage();
+    if(bill.value > 0 ){
+        if(cash.value>=bill.value){
+            const amount = cash.value - bill.value;
+            calculateChange(amount);
 
-    if(billAmtValue>0 && cashGivenValue>0){
-
-        if(billAmtValue > cashGivenValue){
-            errorDiv.innerHTML = "cash is less than bill, enter correct amount";
-            return;
+        }else{
+            sendMessage("Can You wash plates? 😡");
         }
-        calculateNotes(billAmtValue, cashGivenValue);
-    } else{
-        errorDiv.innerHTML = "Ender valid cash or bill amount to continue";
-        }
+    }
+    else{
+        sendMessage("Invalid Bill Amount");
+    }
 })
 
 
-function calculateNotes(billamt, cashamt){
-    let returnAmt = cashamt-billamt;
-    
-    if(returnAmt<1){
-        errorDiv.innerHTML = "No money to be returned";
-        return;
+const calculateChange = (amount) => {
+    for(let i = 0; i < arrayNoteAmt.length; i++){
+        const numberOfNotes = Math.trunc(amount/arrayNoteAmt[i]);
+        amount %= arrayNoteAmt[i];
+        noOfNotes[i].innerText = numberOfNotes;
     }
-
-    for(let i=0; i<arrayNoteAmt.length; i++){
-        returnAmt= compare(returnAmt, arrayNoteAmt[i], i);
-    }
-    
 }
 
-//compare with currency and post the no. of notes on screen
-function compare(remainder, noteAmt, index){
 
-    if(remainder >= noteAmt){
-        let notes = Math.floor(remainder/noteAmt);
-        remainder = remainder - notes*noteAmt;
-        noOfNotes[index].innerText = `${notes}`;
-    }
-    return remainder
+const sendMessage = (message) => {
+    errorDiv.style.display = "block"
+    errorDiv.innerText = message;
 }
 
-//if check button clicked without refreshing the page, clear the no of notes values on the screen
-function clearNoOfNotes(){
-    for(let notes of noOfNotes){
-        notes.innerText = "";
-    }
+const hideMessage = () => {
+    errorDiv.style.display = "none";
 }
